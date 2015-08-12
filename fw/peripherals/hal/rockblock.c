6f1fa1ad0a0d6f02a5414358ae965f1469e3d6f6
Sam Sulaimanov <sam@bitmorse.com>
2015-08-12 22:47:16 +0200

    RB uart now works. local echo on RB had to be off and write/read mode is now binary. timeout on read block is set for now.

--- fw/peripherals/hal/rockblock.c
+++ fw/peripherals/hal/rockblock.c
@@ -5,42 +5,37 @@
  */
 
 #include "../../../Board.h"
-
 #include "rockblock.h"
 
 #define ROCKBLOCK_WAKE 1
 #define ROCKBLOCK_SLEEP 0
-
-#define ROCKBLOCK_RXBUFFER_SIZE 5
-#define ROCKBLOCK_TXBUFFER_SIZE 20
-
-static char rxBuffer[ROCKBLOCK_RXBUFFER_SIZE];
-static char txBuffer[ROCKBLOCK_TXBUFFER_SIZE];
+#define ROCKBLOCK_RXBUFFER_SIZE 20
 
 static UART_Handle uart;
 static UART_Params uartParams;
 
 /* Iridium 9602 Modem AT commands */
 static const char rockblock_at[] = "AT\r";
+static const char rockblock_at_csq[] = "AT+CSQ\r";
 
 
 int rockblock_open(){
 
-
 	//wake rockblock from sleep
     GPIO_write(Board_ROCKBLOCK_SLEEP, ROCKBLOCK_WAKE);
-	cli_printf("rb wakeup",0);
+	cli_printf("RB wake\n",0);
 
-	/* Create a UART with data processing off. */
+	//these uart configs are good for writing to and reading from the RB module
+	//the RB must first be configured to work at 9600baud and local echo off
 	UART_Params_init(&uartParams);
 	uartParams.writeDataMode = UART_DATA_BINARY;
-	uartParams.readDataMode = UART_DATA_TEXT;
+	uartParams.readDataMode = UART_DATA_BINARY;
 	uartParams.readReturnMode = UART_RETURN_FULL;
+	uartParams.readTimeout = 10000;
 	uartParams.readEcho = UART_ECHO_OFF;
 	uartParams.baudRate = 9600;
 
 	uart = UART_open(Board_UART2_COMM, &uartParams);
-	cli_printf("rb uart opened %x",uart);
 
 	if (uart == NULL) {
 		return 0;
@@ -49,26 +44,23 @@ int rockblock_open(){
 	}
 }
 
-int rockblock_begin(){
-	//clear the receive buffer
-	memset(&rxBuffer[0], 0, sizeof(rxBuffer));
 
+
+int rockblock_begin(){
+
+	char rxBuffer[ROCKBLOCK_RXBUFFER_SIZE];
 
 	UART_write(uart, rockblock_at, sizeof(rockblock_at));
+	UART_read(uart, rxBuffer, sizeof(rxBuffer));
 
-	System_printf("wrote to rb");
-	System_flush();
-
-
-	int rbret = UART_read(uart, rxBuffer, sizeof(rxBuffer));
-
-	System_printf("rx'd %d", rbret);
-	System_flush();
-	//cli_printf("rx from rb %d", rbret);
-
-	return 1;
+	if(!strcmp("\r\nOK\r\n", rxBuffer)){
+		return 1; //modem can communicate fine via AT commands
+	}else{
+		return 0;
+	}
 
 }
+
 
 
 void rockblock_close(){
@@ -78,9 +70,12 @@ void rockblock_close(){
 }
 
 
+
 int rockblock_get_sleep_status(){
 	return GPIO_read(Board_ROCKBLOCK_SLEEP);
 }
+
+
 
 int rockblock_get_net_availability(){
 	return GPIO_read(Board_ROCKBLOCK_NET);
