6db688a33d8e5250bf1ea85da57785e3f9f77426
Eloi Benvenuti <eloi.benvenuti@gmail.com>
2015-11-02 16:49:44 +0100

    commit before switching branch

---
+++ fw/peripherals/hal/SHT2x_define.h
@@ -0,0 +1,23 @@
+/*
+ * SHT2x_define.h
+ *
+ *  Created on: Oct 28, 2015
+ *      Author: oronard
+ */
+
+#ifndef FW_PERIPHERALS_HAL_SHT2X_DEFINE_H_
+#define FW_PERIPHERALS_HAL_SHT2X_DEFINE_H_
+
+typedef unsigned char   u8;      ///< range: 0 .. 255
+typedef signed char     s8;      ///< range: -128 .. +127
+
+typedef unsigned short int u16;     ///< range: 0 .. 65535
+typedef signed short int   s16;     ///< range: -32768 .. +32767
+
+//typedef unsigned long   u32;     ///< range: 0 .. 4'294'967'295
+//typedef signed long     s32;     ///< range: -2'147'483'648 .. +2'147'483'647
+
+typedef float           ft;       ///< range: +-1.18E-38 .. +-3.39E+38
+
+
+#endif /* FW_PERIPHERALS_HAL_SHT2X_DEFINE_H_ */
--- fw/peripherals/weather.c
+++ fw/peripherals/weather.c
@@ -10,6 +10,7 @@
 #include "hal/bmp180i2c.h"
 #include "hal/i2c_helper.h"
 #include "../lib/printf.h"
+//#include "hal/SHT2x.h"
 
 
 
@@ -21,6 +22,15 @@ void weather_task(){
 		Task_sleep(3000);
 
 		bme280_data_readout_template();
+
+		//float inside_temperature = sht2x_get_temp();
+		//float inside_humidity = sht2x_get_humidity();
+
+		float inside_temperature = 20.4;
+		float inside_humidity = 0.34;
+
+		cli_printf("temp: %f \n", inside_temperature);
+		cli_printf("RH: %f \n", inside_humidity);
 
 		//bmp180_data_readout_template();
 
