31e9df334f792136aad8495400f40cb83a6106ae
traffael <tschui@kth.se>
2016-02-04 21:39:53 +0100

    added AS5050 basic sensor readout. SPI_transfer fails

--- fw/peripherals/hal/spi_helper.c
+++ fw/peripherals/hal/spi_helper.c
@@ -12,20 +12,21 @@
 //global variable accessible by all tasks performing spi transactions.
 SPI_Handle spi_helper_handle=0;
 
-/*Place this function in every task that needs i2c. It will know by itself if the handle has been opened already*/
+/*Place this function in every task that needs spi. It will know by itself if the handle has been opened already*/
 void spi_helper_init_handle(){
 
 	if(!spi_helper_handle) //only initialize for the first time.
 	{
 		/* Initialise I2C Bus */
 		SPI_Params      params;
+		params.transferTimeout = 100;
 		SPI_Params_init(&params); /*	Defaults values are:
 									transferMode = SPI_MODE_BLOCKING
-									transferTimeout = SPI_WAIT_FOREVER
+									(transferTimeout = SPI_WAIT_FOREVER), overwritten
 									transferCallbackFxn = NULL
 									mode = SPI_MASTER
 									bitRate = 1000000 (Hz)
-									dataSize = 8 (bits)
+									dataSize = 8 (bits); dataSize can range from 4 to 8 bits
 									frameFormat = SPI_POL0_PHA0 */
 		spi_helper_handle = SPI_open(Board_SPI, &params);
 	}
@@ -41,7 +42,7 @@ uint8_t spi_helper_transfer(uint8_t nBytes, uint8_t* txBufferPointer, uint8_t* r
 	static uint8_t ret;
 	ret = SPI_transfer(spi_helper_handle, &spiTransaction);
 	if (!ret) {
-	   // cli_printf("Unsuccessful SPI transfer");
+//	   cli_printf("Unsuccessful SPI transfer");
 	}
 
 	return ret;
