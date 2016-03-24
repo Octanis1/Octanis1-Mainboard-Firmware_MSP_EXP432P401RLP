00ef934e8c4ec5b2c41a5387ef687cf755e5e792
EloiBenvenuti <eloi.benvenuti@gmail.com>
2016-03-24 15:30:02 +0100

    began writing serialization functions

--- fw/core/log.c
+++ fw/core/log.c
@@ -4,10 +4,14 @@
  *  Author: Michael and Eloi
  */
 #include "log.h"
+#include "../peripherals/weather.h"
+#include "../peripherals/gps.h"
+#include "--/peripherals/imu.h"
 
-#define LOGGING_BUFFER_SIZE 100
-
+#define TIMESTAMP_TO_MILLISEC 48000
 /*
+
+TODO:test serialization, write crc8
 
 Logging format:
 - 1byte: length = n
@@ -31,21 +35,81 @@ todo:
 - serialization functions for different sensor values
 */
 
-struct logger *cmp_logger_get(const char *name)
+void logger_make(const char *name)
 {
-    struct logger *l = (struct logger *) logging_buffer_alloc();
+    struct logger log;
 
-    size_t size = LOGGING_BUFFER_SIZE - ((uintptr_t)&l->data + (uintptr_t)l);
-    cmp_mem_access_init(&l->cma, &l->ctx, &l->data, size);
+    cmp_logger_get (name, *log);
+    logger_finish(*log);
+}
+
+void cmp_logger_get(const char *name, struct logger *l)
+{
+    cmp_mem_access_init(&l->cma, &l->ctx, l->data, sizeof(l->buffer));
+    
+    switch(name){
+        case "gps":
+            logging_gps_serialize(l);
+            break;
+        case "imu":
+            logging_imu_serialize(l);
+            break;
+        case "wea":
+            logging_weather_serialize(l);
+            break;
+        default:
+    }
+
 
     //Get number of clock tick since beginning
     //frequency : 48MHz
     uint32_t t = Timestamp_get32();
-    t = t/48000; //converted in ms here
+    t = t/TIMESTAMP_TO_MILLISEC; //converted in ms here
 
     cmp_write_uinteger(&l->ctx, t);
     cmp_write_str(&l->ctx, name);
     return l;
+}
+
+void logging_gps_serialize (struct logger *l){
+    float lat = gps_get_lat();
+    float lon = gps_get_lon();
+    uint8_t fix_qual = gps_get_fix_quality();
+
+    cmp_write_float (&l->ctx, lat);
+    cmp_write_float (&l->ctx, lon);
+    cmp_write_u8 (&l->ctx, fix_qual);
+}
+
+void logging_imu_serialize (struct logger *l){
+    uint8_t imu_calib = imu_get_calib_status();
+    uint16_t imu_head = imu_get_heading();
+    uint16_t imu_roll = imu_get_roll();
+    uint16_t imu_pitch = imu_get_pitch();
+
+    cmp_write_u8 (&l->ctx, imu_calib);
+    cmp_write_u16 (&l->ctx, imu_head);
+    cmp_write_u16 (&l->ctx, imu_roll);
+    cmp_write_u16 (&l->ctx, imu_pitch);
+
+}
+    
+void logging_weather_serialize (struct logger *l){
+
+    unsigned int int_press = weather_get_int_press();
+    int int_temp = weather_get_int_temp();
+    unsigned int int_humi = weather_get_int_humid();
+    unsigned int ex_press = weather_get_ext_press();
+    int ex_temp = weather_get_ex_temp();
+    unsigned int ex_humi = weahter_get_ext_humid();
+
+    cmp_write_u32 (&l->ctx, int_press);
+    cmp_write_s32 (&l->ctx, int_temp);
+    cmp_write_u32 (&l->ctx, int_humi);
+    cmp_write_u32 (&l->ctx, ex_press);
+    cmp_write_s32 (&l->ctx, ex_temp);
+    cmp_write_u32 (&l->ctx, ex_humi);
+
 }
 
 uint8_t logger_finish(struct logger *l)
@@ -68,3 +132,5 @@ uint8_t logger_pop(struct logger *l)
     // check crc
     return ret;
 }
+
+
--- fw/core/log.h
+++ fw/core/log.h
@@ -9,6 +9,8 @@
 #include "../lib/cmp_mem_access/cmp_mem_access.h"
 #include <stdint.h>
 
+#define LOGGING_BUFFER_SIZE 42
+
 /* The following objects are created statically. See app.cfg */
 // extern Semaphore_Handle logging_sem; <---since we don't want to
 // log thing right after they are pushed to the queue, for performance
@@ -17,14 +19,26 @@
 extern Queue_Handle logging_queue;
 
 /* Moved the struct in the .h, not sure if necessary */
+/*Biggest block is currently weather, with 
+ * 6*4 bytes int (6*5)
+ * a 3 char ID name (gps, imu, wea, ...) (+5)
+ * 4 bytes timestamp (+5)
+ * a crc8 (+2)
+ * So the buffer need to be at least 42-bytes long*/
 struct logger {
     cmp_ctx_t ctx;
     cmp_mem_access_t cma;
-    uint8_t data;
+    uint8_t data [LOGGING_BUFFER_SIZE];
 };
 
+//Given a name (gps, wea, etc.), serialize the current data into a buffer
+void logger_make(const char *name);
 
-struct logger *cmp_logger_get(const char *name);
+//Function to serialize the data into a buffer using MessagePack
+void cmp_logger_get(const char *name, struct logger *l);
+void logging_gps_serialize(struct logger *l);
+void logging_imu_serialize(struct logger *l);
+void logging_weather_serialize(struct logger *l);
 
 /*Respectively calculate crc and push the message
  * or pop the message and check crc
