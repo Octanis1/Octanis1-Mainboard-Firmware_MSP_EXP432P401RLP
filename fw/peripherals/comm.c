13f2151d3306992bb3f763fc3f3c1f175f37f455
Sam Sulaimanov <sam@bitmorse.com>
2016-05-28 14:00:27 +0200

    adding other important mavlink messages

--- fw/peripherals/comm.c
+++ fw/peripherals/comm.c
@@ -595,8 +595,8 @@ void comm_task(){
     			rx_counter=0;
 
 			#ifdef MAVLINK_ENABLED
-			/* MAVLINK TESTING START */
-			   mavlink_system_t mavlink_system;
+    			/* MAVLINK GENERIC SETUP */
+    			   mavlink_system_t mavlink_system;
 
 			   mavlink_system.sysid = 25;                   ///< ID 25 for this rover
 			   mavlink_system.compid = MAV_COMP_ID_ALL;     ///< The component sending the message is all, it could be also a Linux process
@@ -605,28 +605,184 @@ void comm_task(){
 			   uint8_t system_type = MAV_TYPE_GROUND_ROVER;
 			   uint8_t autopilot_type = MAV_AUTOPILOT_GENERIC;
 
-			   uint8_t system_mode = MAV_MODE_PREFLIGHT; ///< Booting up
-			   uint8_t system_state = MAV_STATE_UNINIT; ///< System ready for flight
+			   uint8_t system_mode = MAV_MODE_MANUAL_DISARMED; ///< Booting up
+			   uint8_t system_state = MAV_STATE_STANDBY; ///< System ready for flight
+
+			   uint64_t system_time = 1000 * 1000 * (my_rover_status.system_seconds); //TODO get time on each message packing
+
+
 
 
 			   // Initialize the required buffers
 			   mavlink_message_t msg;
 			   uint8_t buf[MAVLINK_MAX_PACKET_LEN];
+			   uint16_t mavlink_msg_len;
 
+
+
+			  /* MAVLINK HEARTBEAT */
 			   // Pack the message
 			   mavlink_msg_heartbeat_pack(mavlink_system.sysid, mavlink_system.compid, &msg, system_type, autopilot_type, system_mode, 0, system_state);
 
-			   // Copy the message to the send buffer
-			   uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+			   // Copy the message to the send buffer and send
+			   mavlink_msg_len = mavlink_msg_to_send_buffer(buf, &msg);
+ 			   cli_send_mavlink(buf, mavlink_msg_len);
 
-			   // Send the message with the standard UART send function
-			   // uart0_send might be named differently depending on
-			   // the individual microcontroller / library in use.
 
- 			   cli_send_mavlink(buf, len);
+  			  /* MAVLINK AUTOPILOT_VERSION */
+			   // Pack the message
+			   mavlink_msg_autopilot_version_pack(mavlink_system.sysid, mavlink_system.compid, &msg,
+										   MAV_PROTOCOL_CAPABILITY_SET_ACTUATOR_TARGET | MAV_PROTOCOL_CAPABILITY_FLIGHT_TERMINATION | MAV_PROTOCOL_CAPABILITY_COMMAND_INT | MAV_PROTOCOL_CAPABILITY_MISSION_INT,
+										   1,
+										   1,
+										   1,
+										   1,
+										   NULL,
+										   NULL,
+										   NULL,
+										   1024,
+										   1,
+										   1
+									   );
 
-			/* MAVLINK TESTING END */
-			#endif
+			   // Copy the message to the send buffer and send
+			   mavlink_msg_len = mavlink_msg_to_send_buffer(buf, &msg);
+			   cli_send_mavlink(buf, mavlink_msg_len);
+
+
+
+ 			  /* MAVLINK GPS_RAW_INT */
+			  mavlink_msg_gps_raw_int_pack(mavlink_system.sysid, mavlink_system.compid, &msg,
+					  	  	  	  	  	  system_time,
+					 					  my_rover_status.gps_fix_quality,
+					  	  	  	  	  	  my_rover_status.gps_lat,
+										  my_rover_status.gps_long,
+										  my_rover_status.altitude,
+										  UINT16_MAX,
+										  UINT16_MAX,
+										  my_rover_status.speed,
+										  UINT16_MAX,
+										  255);
+			  // Copy the message to the send buffer and send
+			  mavlink_msg_len = mavlink_msg_to_send_buffer(buf, &msg);
+			  cli_send_mavlink(buf, mavlink_msg_len);
+
+
+ 			  /* MAVLINK HIGHRES_IMU */
+			  mavlink_msg_highres_imu_pack(mavlink_system.sysid, mavlink_system.compid, &msg,
+					  	  	  	  	  	  system_time,
+										  my_rover_status.accel_x,
+										  my_rover_status.accel_y,
+										  my_rover_status.accel_z,
+										  0, //x gyro
+										  0, //y gyro
+										  0, //z gyro
+										  0, //x mag
+										  0, //y mag
+										  0, //z mag
+										  my_rover_status.int_pressure, //absolute pressure
+										  0, //differential pressure
+										  0, //altitude from pressure
+										  my_rover_status.int_temperature,
+										  0xffff //bitmask for fields that have been updated since last message (0=xacc, 12=temp)
+			  	  	  	  	  	  	  	  );
+			  // Copy the message to the send buffer and send
+			  mavlink_msg_len = mavlink_msg_to_send_buffer(buf, &msg);
+			  cli_send_mavlink(buf, mavlink_msg_len);
+
+
+			  /* MAVLINK ATTITUDE  */
+			  mavlink_msg_attitude_pack(mavlink_system.sysid, mavlink_system.compid, &msg,
+					  0,
+					  my_rover_status.imu_roll,
+					  my_rover_status.imu_pitch,
+					  my_rover_status.imu_heading,
+					  0, //rollspeed
+					  0, //pitchspeed
+					  0  //yawspeed
+			  	  	  );
+
+			  // Copy the message to the send buffer and send
+			  mavlink_msg_len = mavlink_msg_to_send_buffer(buf, &msg);
+			  cli_send_mavlink(buf, mavlink_msg_len);
+
+
+
+			  /* MAVLINK GLOBAL_POSITION_INT */
+			  mavlink_msg_global_position_int_pack(mavlink_system.sysid, mavlink_system.compid, &msg,
+			  					  	  	  	  	  	  0,
+			  					 					  my_rover_status.gps_fix_quality,
+			  					  	  	  	  	  	  my_rover_status.gps_lat,
+			  										  my_rover_status.gps_long,
+			  										  my_rover_status.altitude,
+			  										  0,
+			  										  0,
+			  										  0,
+			  										  my_rover_status.imu_heading);
+
+			  // Copy the message to the send buffer and send
+			  mavlink_msg_len = mavlink_msg_to_send_buffer(buf, &msg);
+			  cli_send_mavlink(buf, mavlink_msg_len);
+
+
+			  /*
+			  											stat->v_bat,
+			  											stat->v_solar,
+			  											stat->i_in,
+			  											stat->i_out,
+			  											stat->im	u_calib_status,
+			  											stat->imu_heading,
+
+			  											stat->int_humidity,
+			  											stat->ext_temperature,
+			  											stat->ext_pressure,
+			  											stat->ext_humidity,
+
+			  											stat->angle_target);
+			  										*/
+
+
+
+
+
+ 			  /* MAVLINK SYS_STATUS */
+			  mavlink_msg_sys_status_pack(mavlink_system.sysid, mavlink_system.compid, &msg,
+					                      0,
+										  0,
+										  0,
+										  0,
+										  my_rover_status.v_bat,
+										  my_rover_status.i_out,
+										  -1,
+										  0,
+										  0,
+										  0,
+										  0,
+										  0,
+										  0
+			  	  	  	  	  	  	  	  );
+
+			  // Copy the message to the send buffer and send
+			  mavlink_msg_len = mavlink_msg_to_send_buffer(buf, &msg);
+			  cli_send_mavlink(buf, mavlink_msg_len);
+
+
+			  /* MAVLINK SYSTEM_TIME */
+			  mavlink_msg_system_time_pack(mavlink_system.sysid, mavlink_system.compid, &msg,
+					  	  	  	  	  	 system_time,
+										 0
+			  	  	  	  	  	  	  	 );
+
+			  // Copy the message to the send buffer and send
+			  mavlink_msg_len = mavlink_msg_to_send_buffer(buf, &msg);
+			  cli_send_mavlink(buf, mavlink_msg_len);
+
+
+
+			  /* MAVLINK STATUS_TEXT (can be used like printf, 50B max) */
+
+
+			  #endif
 
 
 
